775904f3a60b31c60e8ab970893415412ef46e22
spekkie13 <32901490+spekkie13@users.noreply.github.com>
2025-05-05 18:03:15 +0200

    updated api routes

--- constants/APIConstants.tsx
+++ constants/APIConstants.tsx
@@ -5,7 +5,7 @@ export const EXPENSE_BASE_URL = `${API_BASE_URL}/expense`
 export const INCOME_BASE_URL = `${API_BASE_URL}/income`
 export const PERIOD_RESULT_BASE_URL = `${API_BASE_URL}/results`
 export const PERIOD_BUDGET_BASE_URL = `${API_BASE_URL}/budgets`
-export const USER_BASE_URL = `${API_BASE_URL}/users`
+export const USER_BASE_URL = `${API_BASE_URL}/user`
 export const TEAM_BASE_URL = `${API_BASE_URL}/teams`
 export const USER_PREFERENCES_BASE_URL = `${API_BASE_URL}/preferences`
 
